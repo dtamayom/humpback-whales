34005a0253d4c26a942b35f3e63c65f0c4cf2677
Daniela Tamayo <d.tamayo@uniandes.edu.co>
2020-08-04 13:31:52 -0500

    Make image folder from masks

---
+++ DataMasks.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+import os
+import cv2
+
+if not os.path.exists("../data/HumpbackWhales/mask_images"):
+    os.mkdir("../data/HumpbackWhales/mask_images")
+
+mask_path="../data/HumpbackWhales/masks"
+train_path="../data/HumpbackWhales/train/"
+
+c=1
+for mask in os.listdir(mask_path):
+    imagen = cv2.imread(train_path + mask, 1)
+    cv2.imwrite(os.path.join("../data/HumpbackWhales/mask_images/",mask), imagen)
+    print(c)
+    c+=1
